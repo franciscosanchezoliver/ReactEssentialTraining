83b122ee662541fd74d131cd4b418549081eae15
Francisco Sánchez Oliver <francisco.sanchez.external@atos.net>
2019-05-07 06:14:55 +0200

    Creando un app desde 0 y de otra manera

--- react-essential-training/src/App.js
+++ react-essential-training/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Create one react element
+// Arguments of method React.createElement:
+// 1º type of element
+// 2º attributes 
+// 3º child element 
+const title = React.createElement(
+  'h1',   
+  {
+    id:'title',
+    className:'header'
+  },
+  'Hello World'
+)
 
-export default App;
+// Put React element into the DOM
+// Arguments of method ReactDOM.render
+// 1º What do we want to render
+// 2º Where do we want to render this
+ReactDOM.render(
+  title, 
+  document.getElementById('root')
+)
